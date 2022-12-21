bd02209b6b7c0c6a70e62ecaab9499d932bb1417
Theo <theo.sohlman@gmail.com>
2022-12-21 16:25:29 +0100

    Fixat login knapp och felmeddelanden!

--- src/klasser/mittFönster.java
+++ src/klasser/mittFönster.java
@@ -44,6 +44,7 @@ public class mittFönster extends javax.swing.JFrame {
         txtPassword = new javax.swing.JPasswordField();
         andraLosen = new javax.swing.JButton();
         lblRubrik = new javax.swing.JLabel();
+        lblLoginFail = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -77,21 +78,26 @@ public class mittFönster extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(andraLosen)
-                        .addGap(27, 27, 27)
-                        .addComponent(btnLogin))
-                    .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lblRubrik)
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(lblPass)
-                                    .addComponent(lblUser))
+                                .addGap(1, 1, 1)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(lblUser, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(lblPass, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addGap(18, 18, 18)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(txtUsername, javax.swing.GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE)
-                                    .addComponent(txtPassword))))
-                        .addGap(29, 29, 29)))
+                                    .addComponent(txtUsername)
+                                    .addComponent(txtPassword, javax.swing.GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE))))
+                        .addGap(45, 45, 45))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(andraLosen)
+                        .addGap(27, 27, 27)
+                        .addComponent(btnLogin)
+                        .addGap(16, 16, 16))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(1, 1, 1)
+                        .addComponent(lblLoginFail, javax.swing.GroupLayout.PREFERRED_SIZE, 261, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(22, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -103,11 +109,13 @@ public class mittFönster extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblUser, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(txtUsername, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblPass)
-                    .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(15, 15, 15)
+                    .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblPass))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblLoginFail, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(andraLosen))
@@ -118,29 +126,25 @@ public class mittFönster extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
-        ArrayList<HashMap<String,String>> rows = new ArrayList<HashMap<String,String>>();
-        try{
-            String question = "select Namn, Losenord from Agent;";
-            rows = idb.fetchRows(question);
+        if(txtUsername.getText().isEmpty()) {
+            lblLoginFail.setText("Please enter a username and passoword");
         }
-        catch(InfException e){
-            JOptionPane.showMessageDialog(null, "Something went wrong!");
-        }
-        
-        boolean in = false;
-        int i = 0;
-        
-        while(i < rows.size()){
-            HashMap<String,String> nameAndPass = rows.get(i);
-            if((nameAndPass.containsValue(txtUsername.getText())) && (nameAndPass.containsValue(txtPassword.getText()))){
-                JOptionPane.showMessageDialog(null, "Login successful!");
-                in = true;
-                break;
+        else {
+            try {
+                String username = txtUsername.getText();
+                String password = txtPassword.getText();
+                String question = "select Losenord from Agent where Namn = '" + username + "';";
+                String sqlAnswer = idb.fetchSingle(question);
+                if(password.equals(sqlAnswer)) {
+                    lblLoginFail.setText("Du loggade in!");
+                }
+                else {
+                    lblLoginFail.setText("Invalid username or password!");
+                }
             }
-            i++;
-        }
-        if(!in){
-                JOptionPane.showMessageDialog(null, "Access denied!\nInvalid username or password.");
+            catch(InfException ex) {
+                JOptionPane.showMessageDialog(null, "Something went wrong!");
+            }
         }
     }//GEN-LAST:event_btnLoginActionPerformed
 
@@ -152,6 +156,7 @@ public class mittFönster extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton andraLosen;
     private javax.swing.JButton btnLogin;
+    private javax.swing.JLabel lblLoginFail;
     private javax.swing.JLabel lblPass;
     private javax.swing.JLabel lblRubrik;
     private javax.swing.JLabel lblUser;
